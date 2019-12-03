3f53dd8505c7ce5000d2b5299a1301d2c9a258ed
Mathew Gitata <gitataIT@gmail.com>
2019-12-03 15:06:07 +0300

    test: Check that new news articles are assigned ids

---
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -0,0 +1,44 @@
+package dao;
+
+import model.News;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+
+public class Sql2oNewsDaoTest {
+
+    private static Connection conn;
+    private static Sql2oNewsDao newsDao;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/org_test";
+        Sql2o sql2o = new Sql2o(connectionString, "gitata", "gitata");
+        newsDao = new Sql2oNewsDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        newsDao.deleteAll();
+        conn.close();
+    }
+
+    @Test
+    public void add_addingNewsSetsId() throws Exception {
+        News testNews = newTestNews();
+        int originalNewsId = testNews.getId();
+        newsDao.add(testNews);
+
+        assertNotEquals(originalNewsId, testNews.getId());
+    }
+
+
+    private News newTestNews() {
+        return new News("Meeting", "There will be a meeting at 4PM", "General", 0);
+    }
+}
