c26bf29112d872ffc73aad9e8957c4598129b6bb
Mathew Gitata <gitataIT@gmail.com>
2019-12-03 10:56:47 +0300

    Add exception file to capture any API errors

---
+++ src/main/java/exc/ApiErrorException.java
@@ -0,0 +1,15 @@
+package exc;
+
+public class ApiErrorException extends RuntimeException {
+    private final int status;
+
+    public ApiErrorException(int statusCode, String msg) {
+        super(msg);
+        this.status = statusCode;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+}
