47b3f0e59f1bdb119380ec467ca9e6e9379a8312
Mathew Gitata <gitataIT@gmail.com>
2019-12-04 09:54:17 +0300

    Deploy to heroku

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -10,19 +10,27 @@ import org.sql2o.Sql2o;
 import java.util.HashMap;
 import java.util.Map;
 
+import static DB.DB.sql2o;
 import static spark.Spark.*;
 
 public class Api {
     public static void main(String[] args) {
-        staticFileLocation("/public");
-        String connectionString = "jdbc:postgresql://localhost:5432/org";
-        Sql2o sql2o = new Sql2o(connectionString, "gitata", "gitata");
-        Connection conn = sql2o.open();
-        Gson gson = new Gson();
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
 
         UserDao userDao = new Sql2oUserDao(sql2o);
         DepartmentDao departmentDao = new Sql2oDepartmentDao(sql2o);
         NewsDao newsDao = new Sql2oNewsDao(sql2o);
+        Gson gson = new Gson();
 
         //CREATE new user
         post("/users/new-user", "application/json", (req, res) -> {
